7b1d62b4b5efed5160092a81b352e4502f8ddbaf
Zack Sargent <zack@zack.fyi>
2025-09-21 17:39:36 -0400

    allow running from other dirs

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+set -e # stop immediately on errors
+set -o pipefail # do not silently ignore errors in pipelines
+cd "${0%/*}" # cd into script's current location
+
 (cd backend && rails server) &
 (cd frontend && npm run dev) &
 wait
