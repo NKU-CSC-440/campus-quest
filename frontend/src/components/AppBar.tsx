49fcb67763665bff58e0128a1ec74bb04d99c0ec
Drake Woods <woodsd8@nku.edu>
2025-09-30 14:09:33 -0400

    Add dark theme, new layout with AppBar/SideBar, and QuestDashboard UI

---
+++ frontend/src/components/AppBar.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
+type Props = {
+  toggleCollapse: () => void;
+};
+
+export default function MenuAppBar({ toggleCollapse }: Props) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar
+      position="fixed"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <Toolbar>
+        {/* Hamburger toggle */}
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={toggleCollapse}
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Typography variant="h6" noWrap sx={{ flexGrow: 1 }}>
+          Campus Quest
+        </Typography>
+
+        {/* User Menu */}
+        <Box>
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            onClick={handleMenu}
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
