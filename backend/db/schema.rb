9b4f0e6a517d29ab9a580fe7e7fd1fbc56e578de
Zack Sargent <zack@zack.fyi>
2025-10-01 20:41:17 -0400

    add basic support for user sessions

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_29_003813) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_02_003658) do
   create_table "completions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "quest_id", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_29_003813) do
     t.integer "role", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "completions", "quests"
