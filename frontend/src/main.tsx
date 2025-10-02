906e618e3195b260c28c2adee4326222f4791a22
Zack Sargent <zack@zack.fyi>
2025-10-01 21:25:04 -0400

    add user context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import AppTheme from "./components/AppTheme";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <AppTheme>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </AppTheme>
     </HelmetProvider>
   </React.StrictMode>
