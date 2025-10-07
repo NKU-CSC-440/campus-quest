0a7932b43e8bc519d902bd99b87acc48324ff3dd
murphyt5 <murphyt5@nku.edu>
2025-10-07 14:11:19 -0400

    Add files via upload

---
+++ backend/app/controllers/user_controller.rb
@@ -0,0 +1,18 @@
+class UserController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Account Created!"
+        else
+            render :new
+        end 
+    end
+    private
+    def user_params
+        params.require(:user).permit(:email, :password. :password_condirmation)
+    end               
+end
