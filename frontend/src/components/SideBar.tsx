df44cf57db9b1569c4bb1f3c51695d5795561dd6
Zack Sargent <zack@zack.fyi>
2025-11-07 23:40:33 -0500

    remove settings icon

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -7,12 +7,10 @@ import {
   ListItemText,
   Toolbar,
   Divider,
-  Box,
 } from '@mui/material';
 import ReportIcon from '@mui/icons-material/BarChart';
 import QuestIcon from '@mui/icons-material/PriorityHigh';
 import ApprovalsIcon from '@mui/icons-material/Checklist';
-import SettingsIcon from '@mui/icons-material/Settings';
 import GroupsIcon from '@mui/icons-material/Groups';
 import { Link } from 'react-router-dom';
 
@@ -34,17 +32,12 @@ export default function SideBar({ collapsed }: Props) {
           width: collapsed ? collapsedWidth : expandedWidth,
           transition: 'width 0.3s',
           overflowX: 'hidden',
-          display: 'flex',
-          flexDirection: 'column', // 👈 makes it easy to push settings to bottom
-          justifyContent: 'space-between',
         },
       }}
     >
-      {/* Top section */}
-      <Box>
-        <Toolbar />
-        <Divider />
-        <List>
+      <Toolbar />
+      <Divider />
+      <List>
           <ListItem disablePadding sx={{ display: 'block' }}>
             <ListItemButton
               component={Link}
@@ -121,28 +114,6 @@ export default function SideBar({ collapsed }: Props) {
             </ListItemButton>
           </ListItem>
         </List>
-      </Box>
-
-      {/* Bottom section */}
-      <Box>
-        <Divider />
-        <List>
-          <ListItem disablePadding sx={{ display: 'block' }}>
-            <ListItemButton sx={{ justifyContent: collapsed ? 'center' : 'initial', px: 2.5 }}>
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: collapsed ? 'auto' : 2,
-                  justifyContent: 'center',
-                }}
-              >
-                <SettingsIcon />
-              </ListItemIcon>
-              {!collapsed && <ListItemText primary="Settings" />}
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Box>
     </Drawer>
   );
 }
